a52bc3699b375d52b8d5608c2db780be01cd42a7
Han <et801102@gmail.com>
2019-11-12 01:41:41 -0500

    Implment information window for incidents

--- src/IncidentMap.js
+++ src/IncidentMap.js
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { Fragment } from "react";
 import marker from "./marker.svg";
+import twitter from "./ic_twitter.svg";
 import GoogleMapReact from "google-map-react";
 
-const EventMarker = ({ text }) => (
-  <div
-    style={{
-      display: "inline-flex",
-      alignItems: "center",
-      justifyContent: "center",
-      transform: "translate(-50%, -50%)"
-    }}
-  >
-    <img
-      src={marker}
-      className="maker"
-      alt="marker"
-      width="32px"
-      height="32px"
-    />
-  </div>
-);
+const InfoWindow = props => {
+  const { event } = props;
+  const infoWindowStyle = {
+    position: "relative",
+    bottom: 120,
+    left: "-45px",
+    width: 220,
+    backgroundColor: "white",
+    boxShadow: "0 2px 7px 1px rgba(0, 0, 0, 0.3)",
+    alignItems: "left",
+    padding: 10,
+    fontSize: 14,
+    zIndex: 100
+  };
+
+  return (
+    <div style={infoWindowStyle}>
+      <img
+        src={twitter}
+        alt="twitter"
+        style={{ witdh: "32px", height: "32px", float: "left" }}
+      />
+      <div style={{ fontSize: 16 }}>{event.name}</div>
+      <div style={{ fontSize: 12, color: "grey" }}>{event.description}</div>
+    </div>
+  );
+};
+
+const EventMarker = props => {
+  const markerStyle = {
+    display: "inline-flex",
+    alignItems: "center",
+    height: 10,
+    width: 10,
+    justifyContent: "center",
+    transform: "translate(-50%, -50%)"
+  };
+
+  return (
+    <Fragment>
+      <div style={markerStyle}>
+        <img
+          src={marker}
+          className="maker"
+          alt="marker"
+          width="32px"
+          height="32px"
+        />
+      </div>
+      {props.show && <InfoWindow event={props.event} />}
+    </Fragment>
+  );
+};
 
 class IncidentMap extends React.Component {
   static defaultProps = {
@@ -27,14 +63,16 @@ class IncidentMap extends React.Component {
       lat: 40.6892494,
       lng: -74.0466891
     },
-    zoom: 11
+    zoom: 11,
+    map: null,
+    maps: null
   };
 
   constructor(props) {
     super(props);
 
     this.state = {
-      events: null,
+      events: [],
       width: 0,
       height: 0
     };
@@ -44,20 +82,25 @@ class IncidentMap extends React.Component {
     this.setState({ width: window.innerWidth, height: window.innerHeight });
   };
 
+  onChildClick = key => {
+    this.setState(state => {
+      const index = state.events.findIndex(e => e.id === key);
+      state.events[index].show = !state.events[index].show;
+      return { events: state.events };
+    });
+  };
+
   componentDidMount() {
     window.addEventListener("resize", this.updateDimensions);
 
     fetch(process.env.REACT_APP_EVENT_API)
       .then(response => response.json())
-      .then(data =>
-        this.setState({
-          events: data.events.map(event => {
-            const { name, lat, lng } = event;
-
-            return <EventMarker lat={lat} lng={lng} text={name} />;
-          })
-        })
-      );
+      .then(data => {
+        data.events.forEach(result => {
+          result.show = false;
+        });
+        this.setState({ events: data.events });
+      });
   }
 
   componentWillUnmount() {
@@ -73,8 +116,18 @@ class IncidentMap extends React.Component {
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
         defaultCenter={this.props.center}
         defaultZoom={this.props.zoom}
+        onChildMouseEnter={this.onChildMouseEnter}
+        onChildClick={this.onChildClick}
       >
-        {events}
+        {events.map(event => (
+          <EventMarker
+            key={event.id}
+            lat={event.lat}
+            lng={event.lng}
+            show={event.show}
+            event={event}
+          />
+        ))}
       </GoogleMapReact>
     );
   }
